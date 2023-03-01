668e0959527425e3cb847fe2c6f7427be99641c6
Danil <danil.chevgan.00@gmail.com>
2023-03-01 13:13:07 +0600

    hook, useEffect

--- src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,33 +1,48 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
 const ProfileStatusWithHooks = (props) => {
 
+    // Используем useState для установки внутреннего состояния компонента
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    // Используем useEffect для обновления внутреннего состояния компонента при изменении props.status
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
+    // Функция для активации режима редактирования статуса
     const activateEditMode = () => {
-        setEditMode(true)
-    }
+        setEditMode(true);
+    };
+
+    // Функция для деактивации режима редактирования статуса
     const deactivateEditMode = () => {
-        setEditMode(false)
-        props.updateStatus(status);
-    }
+        setEditMode(false);
+        props.updateStatus(status); // Вызываем функцию обновления статуса на сервере
+    };
+
+    // Функция для обновления внутреннего состояния статуса при изменении значения input поля
     const onStatusChange = (e) => {
-        setStatus(e.currentTarget.value)
-    }
+        setStatus(e.currentTarget.value);
+    };
 
     return (
         <div>
             { !editMode &&
+                // Отображаем статус в режиме просмотра
                 <div>
                     <span onDoubleClick={ activateEditMode }> {props.status || "-------"}</span>
                 </div>
             }
             { editMode &&
+                // Отображаем поле для редактирования статуса
                 <div>
                     <input onChange={onStatusChange} autoFocus={true} onBlur={ deactivateEditMode } value={status}/>
                 </div>
             }
         </div>
-    )
-}
+    );
+};
 
 export default ProfileStatusWithHooks;
