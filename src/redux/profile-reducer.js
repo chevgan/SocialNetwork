1b6b0f9d47666accba6889da85260b021eefe3d1
Danil <danil.chevgan.00@gmail.com>
2023-03-01 15:58:21 +0600

    Тесты jest, tdd, тестируем reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,6 +2,7 @@ import {profileAPI, usersAPI} from "../API/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     posts: [
@@ -38,6 +39,8 @@ const profileReducer = (state = initialState, action) => {
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
+        case DELETE_POST:
+            return {...state, posts: state.posts.filter(p => p.id != action.postId)}
         default:
             return state;
     }
@@ -47,7 +50,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
-
+export const deletePost = (postId) => ({type: DELETE_POST, postId})
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId).then(response => {
         dispatch(setUserProfile(response.data));
